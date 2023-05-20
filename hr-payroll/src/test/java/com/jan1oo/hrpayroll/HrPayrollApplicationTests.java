04a0c317a989764cc29b3af7fec505b109d886c1
Jânio Filho <filhojanio67@gmail.com>
2023-05-20 19:59:58 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/jan1oo/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan1oo.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
