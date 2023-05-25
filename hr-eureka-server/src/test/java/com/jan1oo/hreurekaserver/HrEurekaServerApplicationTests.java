45dd959c078bac181160134e5de0c3beb988613b
Jânio Filho <filhojanio67@gmail.com>
2023-05-24 22:55:51 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/jan1oo/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan1oo.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
