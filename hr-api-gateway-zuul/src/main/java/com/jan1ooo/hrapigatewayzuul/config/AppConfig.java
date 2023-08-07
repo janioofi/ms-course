1c4494a67f2c87f4bc5028bfacbb5a151f905a45
jan1ooo <filhojanio67@gmail.com>
2023-08-07 19:42:38 -0300

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/jan1ooo/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/jan1ooo/hrapigatewayzuul/config/AppConfig.java
@@ -1,22 +1,28 @@
 package com.jan1ooo.hrapigatewayzuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 public class AppConfig {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
+	@Bean
+	JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		return tokenConverter;
+	}
 
-	 @Bean
-	    JwtAccessTokenConverter accessTokenConverter() {
-	    	JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-	    	tokenConverter.setSigningKey("12345678");
-	    	return tokenConverter;
-	    }
-	    
-	    @Bean
-	    JwtTokenStore tokenStore() {
-	    	return new JwtTokenStore(accessTokenConverter());
-	    }
+	@Bean
+	JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 }
